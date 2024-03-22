a8aaec2e814226796004e88ee525dcc185a7bac6
sgc6512 <81432815+sgc6512@users.noreply.github.com>
2024-03-22 19:43:01 +0000

    fixed rotation issues
    Cleaned up the tweens a bit, fixed the rotation rounding issue

--- player.gd
+++ player.gd
@@ -10,27 +10,28 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _physics_process(_delta):
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	
 	# Decided to stick with tweening. We can handle going up slopes or falling down as edge cases
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction and !moving:
 		var tween = create_tween()
 		moving = true
-		tween.tween_property(player, "position", position + direction, 0.5)
+		tween.tween_property(player, "position", direction, 0.5).as_relative().set_trans(
+			Tween.TRANS_SINE)
 		tween.connect("finished", on_tween_finished)
 	
-	# Handle rotation, there seems to be a rounding issue when looking left then right
+	# Handle rotation
 	if Input.is_action_just_released("rotate_left") and !moving:
 		var tween = create_tween()
 		moving = true
-		tween.tween_property(player, "rotation:y", rotation.y + deg_to_rad(90), 0.5)
+		tween.tween_property(player, "rotation_degrees:y", 90, 0.5).as_relative().set_trans(
+			Tween.TRANS_SINE)
 		tween.connect("finished", on_tween_finished)
 	if Input.is_action_just_released("rotate_right") and !moving:
 		var tween = create_tween()
 		moving = true
-		tween.tween_property(player, "rotation:y", rotation.y + deg_to_rad(-90), 0.5)
+		tween.tween_property(player, "rotation_degrees:y", -90, 0.5).as_relative().set_trans(
+			Tween.TRANS_SINE)
 		tween.connect("finished", on_tween_finished)
 	
 	# Potentially not needed as we are not using the velocity
@@ -39,4 +40,3 @@ func _physics_process(_delta):
 
 func on_tween_finished():
 	moving = false
-	print(rotation_degrees)
